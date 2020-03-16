65b050d32bb861a3579f16b64bf55ec05f4f79ee
Mamkh27 <mkheer@ucdavis.edu>
2020-03-15 22:33:15 -0700

    Add files via upload

--- actions.js
+++ actions.js
@@ -1,6 +1,7 @@
 
 // var button = document.getElementByClass('b4'); // Assumes element with id='button'
 document.write("Hello Guys");
+alert("alertttttt");
 //     var div = document.getElementByClass('ig_stuff');
 //     var div2 = document.getElementByClass('test')
 //     if (div.style.display !== 'none') {

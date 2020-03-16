0bfd7ba37b77921629769e9f5c52341e5ea1cd26
Mamkh27 <mkheer@ucdavis.edu>
2020-03-15 22:46:50 -0700

    Add files via upload

--- actions.js
+++ actions.js
@@ -1,15 +1,15 @@
 
-// var button = document.getElementByClass('b4'); // Assumes element with id='button'
+ var button = document.getElementByClass('b4'); // Assumes element with id='button'
 document.write("Hello Guys");
 alert("alertttttt");
-//     var div = document.getElementByClass('ig_stuff');
-//     var div2 = document.getElementByClass('test')
-//     if (div.style.display !== 'none') {
-//         div.style.display = 'none';
-//         div2.style.display = 'none';
-//     }
-//     else {
-//         div.style.display = 'inline';
-//         div2.style.display = 'inline';
-//     }
-// };
+    var div = document.getElementById('pixlee_container');
+    var div2 = document.getElementByClass('test')
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+        div2.style.display = 'none';
+    }
+    else {
+        div.style.display = 'inline';
+        div2.style.display = 'inline';
+    }
+};

1a74c5c5e26bde8b964011011fc3cc63ab6bcfe3
Mamkh27 <mkheer@ucdavis.edu>
2020-03-15 22:22:40 -0700

    added js file

---
+++ actions.js
@@ -0,0 +1,14 @@
+
+var button = document.getElementByClass('b4'); // Assumes element with id='button'
+document.write("Hello Guys");
+    var div = document.getElementByClass('ig_stuff');
+    var div2 = document.getElementByClass('test')
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+        div2.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+        div2.style.display = 'block';
+    }
+};
